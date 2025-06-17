06de0c311549b1500e8188c7a45f1febad6f95a2
Rahul Kumar <rahulkumar20092002rk@gmail.com>
2025-06-17 20:49:26 +0530

    top_adder.v

--- top_adder.v
+++ top_adder.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.09.2022 16:15:12
-// Design Name: 
-// Module Name: top_adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module top_adder(
     input [3:0] InA,
     input [3:0] InB,
