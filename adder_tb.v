4f8bccb206194f9abb50b0454beafb84bb4c657f
Rahul Kumar <rahulkumar20092002rk@gmail.com>
2025-06-17 20:50:18 +0530

    adder_tb.v

--- adder_tb.v
+++ adder_tb.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.09.2022 16:31:23
-// Design Name: 
-// Module Name: adder_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module adder_tb(
     );
     
