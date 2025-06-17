34f573191b45703188a2239145ec07e194be78d2
Rahul Kumar <rahulkumar20092002rk@gmail.com>
2025-06-17 20:49:56 +0530

    full_adder.v

--- full_adder.v
+++ full_adder.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.09.2022 16:15:41
-// Design Name: 
-// Module Name: full_adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module full_adder_1bit(
     input FA1_InA,
     input FA1_InB,
